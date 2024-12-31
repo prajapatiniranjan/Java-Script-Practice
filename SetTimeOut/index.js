177e58db44ebd1b0160e18a32521312829ff377a
Niranjan Prajapati <81921820+prajapatiniranjan@users.noreply.github.com>
2024-12-31 19:22:16 +0530

    SetTimeOut

---
+++ SetTimeOut/index.js
@@ -0,0 +1,14 @@
+
+function settout() {
+    setTimeout(show, 3000);
+
+
+
+function show() {
+    let a = document.querySelector('#website');
+    a.style.display = "block";
+
+    let b = document.querySelector('#img');
+    b.style.display = "none";
+}
+}
